d0ed37d40de7403fcdf19abe9051af88ab7111cd
Mark Lyle <mlyle@cheapcontrols.com>
2022-03-10 21:18:32 -0600

    catch up on changes

--- js/default.js
+++ js/default.js
@@ -11,10 +11,14 @@ let ml_account = ml('accounts', '1264578', 'o0x0s5n1h4', 'load');
 
 
 // Google Function
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+(function(i,s,o,g,r,a,m){
+	i['GoogleAnalyticsObject']=r;
+		i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];
+		a.async=1;
+		a.src=g;
+		m.parentNode.insertBefore(a,m)
+})
+	(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 	ga('create', 'UA-39236557-9', 'auto');
 	ga('send', 'pageview');
  // End Google Function
